367e5eabec0954ca58141ede4c1cc44804be4b0f
Shourov98 <mdmahbub0412@gmail.com>
2025-05-17 18:13:04 +0600

    Update CORS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 
 
 app.use(cors({
-  origin: 'http://localhost:3000',  // your frontend URL
+  origin: '*',  // your frontend URL
   credentials: true,                 // if you use cookies or auth headers
 }));
 
